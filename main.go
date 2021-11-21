e1031f117fa2edc1e4a201a06090224908c16e3f
hisyntax <hisyntax0@gmail.com>
2021-11-21 17:32:36 +0100

    Static is served

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl *template.Template
+
+type Todo struct {
+	Item string
+	Done bool
+}
+
+type PageData struct {
+	Title string
+	Todos []Todo
+}
+
+func todo(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title: "TODO LIST",
+		Todos: []Todo{
+			{Item: "Install GO", Done: true},
+			{Item: "Learn GO", Done: false},
+			{Item: "Check the codebase", Done: false},
+		},
+	}
+	tmpl.Execute(w, data)
+}
 
 func main() {
-	fmt.Println("Todo list")
+	mux := http.NewServeMux()
+	tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("/todo", todo)
+
+	fmt.Println("Server running")
+	log.Fatal(http.ListenAndServe(":8000", mux))
 }
